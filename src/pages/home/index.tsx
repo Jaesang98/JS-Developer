a53c70d12649953db63a729fe18995d56bb03216
jsnam <skawotkd12@naver.com>
2025-03-05 17:48:39 +0900

    chore(jsnam) : 폴더구조 수정

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -4,7 +4,20 @@ import MainLayout from '@/components/MainLayout';
 function Home() {
     return (
         <MainLayout>
-            <div>aa</div>
+            <div className="home-contentWrap">
+                <div className="home-content">
+                    <div className="home-title">JS-Developer</div>
+                    <div className="home-subtitle">
+                        Though it looks quite polished in English, this is still a developer site with much room for improvement.
+                    </div>
+                    <div className="home-hr"></div>
+                    <div className="home-btnWrap">
+                        <button className="btn2">시작하기</button>
+                        <button className="btn3">개발 가이드</button>
+                        <button className="btn3">개발 사전</button>
+                    </div>
+                </div>
+            </div>
         </MainLayout>
     );
 }
