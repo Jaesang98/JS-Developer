f0a6699bbf12b185a7dfed7a37460c68aee19cce
jsnam <skawotkd12@naver.com>
2025-03-13 18:14:40 +0900

    chore(jsnam) : 개발자사전 시작

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,5 +1,6 @@
 import { Routes, Route, Navigate } from 'react-router-dom';
 import Home from '@/pages/home';
+import Devx from '@/pages/devx';
 import Error from '@/pages/error';
 
 const Router = () => {
@@ -8,6 +9,7 @@ const Router = () => {
             <Routes>
                 <Route path="/" element={<Navigate to="/Home" />} />
                 <Route path="/Home" element={<Home />} />
+                <Route path="/Devx" element={<Devx />} />
                 <Route path="*" element={<Error />} />
             </Routes>
         </>
