dcb6ee388a1d861a74f565439c84a19a43007cb5
jsnam <skawotkd12@naver.com>
2025-03-13 17:27:37 +0900

    chore(jsnam) : 모듈화 및 다크모드 수정

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,17 +1,17 @@
 import MainLayout from '@/components/MainLayout';
-import '@/assets/styles/pages/home.module.scss';
+import styles from '@/assets/styles/pages/home.module.scss';
 
 function Home() {
     return (
         <MainLayout>
-            <div className="home-contentWrap">
-                <div className="home-content">
-                    <div className="home-title">JS-Developer</div>
-                    <div className="home-subtitle">
+            <div className={styles['home-contentWrap']}>
+                <div className={styles['home-content']}>
+                    <div className={styles['home-title']}>JS-Developer</div>
+                    <div className={styles['home-subtitle']}>
                         Though it looks quite polished in English, this is still a developer site with much room for improvement.
                     </div>
-                    <div className="home-hr"></div>
-                    <div className="home-btnWrap">
+                    <div className={styles['home-hr']}></div>
+                    <div className={styles['home-btnWrap']}>
                         <button className="btn2">시작하기</button>
                         <button className="btn3">개발 가이드</button>
                         <button className="btn3">개발 사전</button>
