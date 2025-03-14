76be98d8743c76085f18abf491b3e09fcc1dc8e6
jsnam <skawotkd12@naver.com>
2025-03-14 16:13:43 +0900

    chore(jsnam) : 인풋적용

--- src/pages/devx/index.tsx
+++ src/pages/devx/index.tsx
@@ -1,5 +1,6 @@
 import MainLayout from '@/components/layout/MainLayout';
 import styles from '@/assets/styles/pages/devx.module.scss';
+import Input from '@/components/ui/input';
 
 function Devx() {
     return (
@@ -7,6 +8,9 @@ function Devx() {
             <div className={styles['devx-contentWrap']}>
                 <div className={styles['devx-content']}>
                     <div className={styles['devx-title']}>개발자 사전</div>
+                    <div className={styles['devx-filterWrap']}>
+                        <Input variant="input1" children={undefined}></Input>
+                    </div>
                 </div>
             </div>
         </MainLayout>
