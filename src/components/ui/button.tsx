5187a90c4d173a0a087356b6d3872403ea098850
jsnam <skawotkd12@naver.com>
2025-03-14 16:27:49 +0900

    chore(jsnam) : devx-filterWrap 여기부터 하기

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import styles from '@/assets/styles/components/ui/button.module.scss';
 
 interface ButtonProps {
-    variant?: 'btn1' | 'btn2' | 'btn3';
+    variant?: 'btn1' | 'btn2' | 'btn3' | 'btn4';
     children: React.ReactNode;
     onClick?: () => void;
     disabled?: boolean;
@@ -12,6 +12,7 @@ interface ButtonProps {
  * - btn1: 로그인 버튼
  * - btn2: 메인화면 시작하기 버튼
  * - btn3: 메인화면 보러가기 버튼
+ * - btn4: 개발자 사전 검색 버튼
  */
 const Button: React.FC<ButtonProps> = ({ variant = 'btn1', children, onClick, disabled = false }) => {
     return (
--- src/pages/devx/index.tsx
+++ src/pages/devx/index.tsx
@@ -1,6 +1,7 @@
 import MainLayout from '@/components/layout/MainLayout';
 import styles from '@/assets/styles/pages/devx.module.scss';
 import Input from '@/components/ui/input';
+import Button from '@/components/ui/button';
 
 function Devx() {
     return (
@@ -8,8 +9,15 @@ function Devx() {
             <div className={styles['devx-contentWrap']}>
                 <div className={styles['devx-content']}>
                     <div className={styles['devx-title']}>개발자 사전</div>
-                    <div className={styles['devx-filterWrap']}>
-                        <Input variant="input1" children={undefined}></Input>
+                    <div className={styles['devx-filter']}>
+                        <div className={styles['devx-searchWrap']}>
+                            <Input variant="input1" children={undefined}></Input>
+                            <Button variant="btn4">검색</Button>
+                        </div>
+                        <div className={styles['devx-filterWrap']}>
+                            aaa
+                            <Button variant="btn4">검색</Button>
+                        </div>
                     </div>
                 </div>
             </div>
