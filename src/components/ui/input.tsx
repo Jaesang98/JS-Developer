5ba36e7d3372695ed8e2ef4c1c45e35725db7db5
jsnam <skawotkd12@naver.com>
2025-03-14 16:12:58 +0900

    chore(jsnam) : input 생성

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -1,13 +1,22 @@
-function Footer() {
-    return (
-        <div className="footer">
-            <div className="footer-title">JS-Developer</div>
-            <div className="footer-info">
-                <div className="footer-item">email : skawotkd12@naver.com</div>
-                <div className="footer-item">© 2025 Nam Jaesang. All Rights Reserved </div>
-            </div>
-        </div>
-    );
+import React from 'react';
+import styles from '@/assets/styles/components/ui/input.module.scss';
+
+interface InputProps {
+    variant?: 'input1' | 'input2' | 'input3';
+    children: React.ReactNode;
+    onClick?: () => void;
+    disabled?: boolean;
 }
 
-export default Footer;
+/**
+ * - input1: 개발자 사전 인풋
+ */
+const Input: React.FC<InputProps> = ({ variant = 'input1', children, onClick, disabled = false }) => {
+    return (
+        <input className={`${styles[variant]}`} onClick={onClick} disabled={disabled}>
+            {children}
+        </input>
+    );
+};
+
+export default Input;
