f3611e465a311839cbb84cc3bb9d1be8ed3c7680
jsnam <skawotkd12@naver.com>
2025-03-15 18:53:51 +0900

    chore(jsnam) : 개발자 사전 퍼블리싱 거의 개발 페이지 네이션 해야함

---
+++ src/components/ui/card.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import styles from '@/assets/styles/components/ui/card.module.scss';
+
+interface CardProps {
+    title: String;
+    content: String;
+    name: String;
+    date: String;
+    children: React.ReactNode;
+}
+
+/**
+ * - btn1: 로그인 버튼
+ * - btn2: 메인화면 시작하기 버튼
+ * - btn3: 메인화면 보러가기 버튼
+ * - btn4: 개발자 사전 검색 버튼
+ * - btn5: 개발자 사전 단어 추가 버튼
+ */
+const Card: React.FC<CardProps> = ({ title, content, name, date, ...props }) => {
+    return (
+        <div className={styles['cardwrap']}>
+            <div className={styles['card-title']}>{title}</div>
+            <div className={styles['card-content']}>{content}</div>
+            <div className={styles['card-info']}>
+                <div className={styles['card-name']}>{name}</div>
+                <div className={styles['card-date']}>{date}</div>
+            </div>
+        </div>
+    );
+};
+
+export default Card;
--- src/pages/devx/index.tsx
+++ src/pages/devx/index.tsx
@@ -3,6 +3,7 @@ import styles from '@/assets/styles/pages/devx.module.scss';
 import Input from '@/components/ui/input';
 import Button from '@/components/ui/button';
 import Switch from '@/components/ui/switch';
+import Card from '@/components/ui/card';
 
 function Devx() {
     return (
@@ -19,6 +20,18 @@ function Devx() {
                             <Switch></Switch>
                             <Button variant="btn5">단어 추가</Button>
                         </div>
+                    </div>
+                    <div className={styles['devx-card']}>
+                        {Array.from({ length: 8 }, (_, index) => (
+                            <Card
+                                key={index}
+                                title={`Title ${index + 1}`}
+                                content={`이것은 ${index + 1}번째 카드의 내용입니다. 위에서 src경로에 이미지 파일이 있을 때는 상대경로로 지정하는 것이 안 되어서 두 가지 예시 방법을 사용했다면, public에 있을 때는 다르다.`}
+                                name={`작성자 ${index + 1}`}
+                                date={`2024-12-${30 - index}`}
+                                children={undefined}
+                            />
+                        ))}
                     </div>
                 </div>
             </div>
