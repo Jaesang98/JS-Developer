b42ad3bfc15402efb407889c03d9be92152b5e41
jsnam <skawotkd12@naver.com>
2025-04-15 20:21:47 +0900

    chore(jsnam) : 회원가입입

--- src/pages/auth/create.tsx
+++ src/pages/auth/create.tsx
@@ -13,18 +13,18 @@ function Create() {
                     <div className={styles['register-input']}>
                         <div className={styles['edit-description']}>아이디</div>
                         <Input placeholder={'아이디'}></Input>
-                        <div className="warning">* 대소문자~~</div>
-                        <div className="success">잘 됨</div>
+                        <div className="warning">* 이미 사용 중인 아이디입니다.</div>
+                        <div className="success">사용 가능한 아이디입니다.</div>
                     </div>
 
                     <div className={styles['register-input']}>
                         <div className={styles['edit-description']}>새 비밀번호</div>
                         <Input placeholder={'비밀번호'}></Input>
-                        <div className="warning">* 대소문자~~</div>
-                        <div className="success">잘 됨</div>
+                        <div className="warning">* 영문 대/소문자, 숫자, 특수문자를 모두 포함해야 합니다.</div>
+                        <div className="success">안전한 비밀번호입니다.</div>
                         <Input placeholder={'비밀번호 확인'} disabled={true}></Input>
-                        <div className="warning">* 대소문자~~</div>
-                        <div className="success">잘 됨</div>
+                        <div className="warning">* 비밀번호가 일치하지 않습니다.</div>
+                        <div className="success">비밀번호가 일치합니다.</div>
                     </div>
 
                     <div className={styles['login-btn']}>
