d1976d9079f9362d266189bd92829026677344d5
jsnam <skawotkd12@naver.com>
2025-05-27 19:24:36 +0900

    chore(jsnam) : 잘바뀌는지 테스트트

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -4,25 +4,26 @@ import MainLayout from '@/components/layout/MainLayout';
 import Button from '@/components/ui/button';
 
 function Home() {
-    const navigate = useNavigate();
-    return (
-        <MainLayout>
-            <div className={styles['home-contentWrap']}>
-                <div className={styles['home-content']}>
-                    <div className={styles['home-title']}>JS-Developer</div>
-                    <div className={styles['home-subtitle']}>
-                        Though it looks quite polished in English, this is still a developer site with much room for improvement.
-                    </div>
-                    <div className={styles['home-hr']}></div>
-                    <div className={styles['home-btnWrap']}>
-                        <Button variant={'btn2'} children={'시작하기'} onClick={() => navigate('/Guide')}></Button>
-                        <Button variant={'btn3'} children={'개발 가이드'} onClick={() => navigate('/Guide')}></Button>
-                        <Button variant={'btn3'} children={'개발 사전'} onClick={() => navigate('/Devx')}></Button>
-                    </div>
-                </div>
-            </div>
-        </MainLayout>
-    );
+  const navigate = useNavigate();
+  return (
+    <MainLayout>
+      <div className={styles['home-contentWrap']}>
+        <div className={styles['home-content']}>
+          <div className={styles['home-title']}>JS-Developer</div>
+          <div className={styles['home-subtitle']}>
+            HI! Though it looks quite polished in English, this is still a developer site with much room for
+            improvement.
+          </div>
+          <div className={styles['home-hr']}></div>
+          <div className={styles['home-btnWrap']}>
+            <Button variant={'btn2'} children={'시작하기'} onClick={() => navigate('/Guide')}></Button>
+            <Button variant={'btn3'} children={'개발 가이드'} onClick={() => navigate('/Guide')}></Button>
+            <Button variant={'btn3'} children={'개발 사전'} onClick={() => navigate('/Devx')}></Button>
+          </div>
+        </div>
+      </div>
+    </MainLayout>
+  );
 }
 
 export default Home;
